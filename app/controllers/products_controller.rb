2dd73af7a7d1832da3af7e5a7120b9d5dd51779f
kerrison24 <kerrisongarcia@gmail.com>
2015-07-01 16:16:06 +1000

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,38 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:title, :description, :image_url, :price)
+    end
+end
