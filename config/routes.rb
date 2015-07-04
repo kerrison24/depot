c26724c12cd8f3e44bf02398f523cdfffdff4605
kerrison24 <kerrisongarcia@gmail.com>
2015-07-04 15:07:00 +1000

    create cart model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'cart/index'
+
   get 'store/index'
   get 'products' => "products#index"
 
   resources :products
+  resources :carts
   root 'store#index', as: 'store'
 end
