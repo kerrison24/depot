99696e8ca5d00e85f0568693bdb44c2b1ee98126
kerrison24 <kerrisongarcia@gmail.com>
2015-07-05 15:28:16 +1000

    add quantity column to cart table, add ability to empty cart and update cart show view

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,24 @@
 class CartsController < ApplicationController
-
+  include CurrentCart
+  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   def show
     @cart = Cart.find(params[:id])
   end
+
+  def destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
+    respond_to do |format|
+      format.html { redirect_to store_url,
+        notice: 'Your cart is currently empty' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    end
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,20 +5,23 @@ class LineItemsController < ApplicationController
 
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
     session[:counter] = nil
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.'}
+        format.html { redirect_to @line_item.cart }
         format.json { render action: 'show',
-          status: :created, location: @line_item}
+          status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
         format.json { render json: @line_item.errors,
           status: :unprocessable_entity }
       end
     end
+  end
+
+  def line_item_params
+    params.require(:line_item).permit(:product_id)
   end
 end
